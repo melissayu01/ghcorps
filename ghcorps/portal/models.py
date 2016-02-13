791e959aa30545f4efc3efeecf226375258db597
frtennis1 <frtennis1@gmail.com>
2016-02-13 01:37:24 -0500

    relative times

--- ghcorps/portal/models.py
+++ ghcorps/portal/models.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 import django.contrib.auth.models as authmodels
+import datetime
 
 # Create your models here.
 
@@ -16,6 +17,11 @@ class User (authmodels.User):
     twitter = models.CharField(max_length = 200)
     about = models.TextField()
     prof_pic = models.ImageField(upload_to=rename_file, max_length=300)
+
+    @property
+    def full_name(self):
+        return ' '.join(first_name, last_name)
+    
 
 class Job (models.Model):
     title = models.CharField(max_length=150)
@@ -35,16 +41,82 @@ class Post (models.Model):
     user = models.ForeignKey(User)
     dt = models.DateTimeField(auto_now_add = True)
 
+    @property
+    def rel_time(self):
+        now = datetime.datetime.now()
+        months = ['Jan', 'Feb', 'Mar', 'Apr',
+          'May', 'Jun', 'Jul', 'Aug',
+          'Sept', 'Oct', 'Nov', 'Dec']
+        diff = now - dt
+        if diff < datetime.timedelta(minutes=1):
+            return "%i seconds" % diff.seconds
+        elif diff < datetime.timedelta(minutes=60):
+            return "%i minutes" % diff.minutes
+        elif diff < datetime.timedelta(hours=24)
+            return "%i hours" % diff.hours
+        elif diff < datetime.timedelta(days=31)
+            return "%i days" % diff.days
+        elif now.year == dt.year
+            return "%s %i" % (months[dt.month], dt.day)
+        else:
+            return "%s %i, %i" % (months[dt.month], 
+                                  dt.day,
+                                  dt.year)
+
 class Reply (models.Model):
     father = models.ForeignKey(Post)
     content = models.TextField()
     user = models.ForeignKey(User)
     dt = models.DateTimeField(auto_now_add = True)
 
+    @property
+    def rel_time(self):
+        now = datetime.datetime.now()
+        months = ['Jan', 'Feb', 'Mar', 'Apr',
+          'May', 'Jun', 'Jul', 'Aug',
+          'Sept', 'Oct', 'Nov', 'Dec']
+        diff = now - dt
+        if diff < datetime.timedelta(minutes=1):
+            return "%i seconds" % diff.seconds
+        elif diff < datetime.timedelta(minutes=60):
+            return "%i minutes" % diff.minutes
+        elif diff < datetime.timedelta(hours=24)
+            return "%i hours" % diff.hours
+        elif diff < datetime.timedelta(days=31)
+            return "%i days" % diff.days
+        elif now.year == dt.year
+            return "%s %i" % (months[dt.month], dt.day)
+        else:
+            return "%s %i, %i" % (months[dt.month], 
+                                  dt.day,
+                                  dt.year)
+
 class Activity (models.Model):
     user = models.ForeignKey(User)
     text = models.TextField()
     dt = models.DateTimeField(auto_now_add = True)
+
+    @property
+    def rel_time(self):
+        now = datetime.datetime.now()
+        months = ['Jan', 'Feb', 'Mar', 'Apr',
+          'May', 'Jun', 'Jul', 'Aug',
+          'Sept', 'Oct', 'Nov', 'Dec']
+        diff = now - dt
+        if diff < datetime.timedelta(minutes=1):
+            return "%i seconds" % diff.seconds
+        elif diff < datetime.timedelta(minutes=60):
+            return "%i minutes" % diff.minutes
+        elif diff < datetime.timedelta(hours=24)
+            return "%i hours" % diff.hours
+        elif diff < datetime.timedelta(days=31)
+            return "%i days" % diff.days
+        elif now.year == dt.year
+            return "%s %i" % (months[dt.month], dt.day)
+        else:
+            return "%s %i, %i" % (months[dt.month], 
+                                  dt.day,
+                                  dt.year)
 
 class Application (models.Model):
     job = models.ForeignKey(Job)
